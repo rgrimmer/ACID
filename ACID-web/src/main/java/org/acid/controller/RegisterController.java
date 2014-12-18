9cd097d5e09fd2de961facecaff3a615fc7981cd
Junior Dussouillez <junior.dussouillez@gmail.com>
2014-12-18 17:38:03 +0100

    Add POST method to /register

--- ACID-web/src/main/java/org/acid/controller/RegisterController.java
+++ ACID-web/src/main/java/org/acid/controller/RegisterController.java
@@ -1,14 +1,34 @@
 package org.acid.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RegisterController {
 
     @RequestMapping("/register")
-    public String login(Model model) {
+    public String register(Model model) {
         return "register";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ModelAndView registerPost(HttpServletRequest request,
+                               @RequestParam(value = "inputUsername", required = true) String inputUsername,
+                               @RequestParam(value = "inputEmail", required = true) String inputEmail,
+                               @RequestParam(value = "inputPassword", required = true) String inputPassword,
+                               @RequestParam(value = "inputPasswordConfirmation", required = true) String inputPasswordConfirmation) {
+        System.out.println(inputUsername);
+        System.out.println(inputEmail);
+        System.out.println(inputPassword);
+        System.out.println(inputPasswordConfirmation);
+        ModelAndView mv = new ModelAndView("register");
+        mv.addObject("errorMsg", "ERREUR !!!!!!");
+        return mv;
     }
 }
