1396310e55038be315baa74946e8118e406a4ffd
Maxime Tournier <steaker90@gmail.com>
2015-01-06 16:13:46 +0100

    Add redirection while no connected, create form for new project

--- ACID-web/src/main/java/org/acid/controller/BoardController.java
+++ ACID-web/src/main/java/org/acid/controller/BoardController.java
@@ -1,6 +1,8 @@
 package org.acid.controller;
 
 import javax.ejb.EJB;
+import javax.servlet.http.HttpSession;
+import org.acid.ejb.entities.User;
 import org.acid.ejb.entitymanager.EntityManager;
 import org.acid.ejb.logger.Logger;
 import org.springframework.stereotype.Controller;
@@ -17,7 +19,10 @@ public class BoardController {
     private Logger logger;
 
     @RequestMapping("/board")
-    public String board(Model model) {
+    public String board(Model model, HttpSession request) {
+        if (((User) request.getAttribute("user")) == null) {
+            return "redirect:/login";
+        }
         return "board";
     }
 
---
+++ ACID-web/src/main/java/org/acid/controller/NewBoardController.java
@@ -0,0 +1,44 @@
+package org.acid.controller;
+
+import javax.ejb.EJB;
+import javax.servlet.http.HttpServletRequest;
+import org.acid.ejb.entitymanager.EntityManager;
+import org.acid.ejb.entitymanager.EntityManagerImpl;
+import org.acid.ejb.logger.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class NewBoardController {
+
+    public static final int MAX_PROJECT_NAME_LENGTH = 256;
+    
+    @EJB(mappedName = "logger")
+    private Logger logger;
+    
+    @EJB(mappedName = "entityManager")
+    private EntityManager entityManager;
+
+    @RequestMapping("/newBoard")
+    public String register(Model model) {
+        return "newBoard";
+    }
+
+    @RequestMapping(value = "/newBoard", method = RequestMethod.POST)
+    public ModelAndView registerPost(HttpServletRequest request,
+                                     @RequestParam(value = "inputProjectName", required = true) String inputProjectName) {
+        System.out.println(inputProjectName);
+        ModelAndView mv = new ModelAndView("newBoard");
+        if(inputProjectName.length() > MAX_PROJECT_NAME_LENGTH){
+            mv.addObject("errorMsg", "Project name  must be less than 256 caracters");
+            return mv;
+        }        
+        mv = new ModelAndView("newBoard");
+        return (mv);
+        
+    }
+}
