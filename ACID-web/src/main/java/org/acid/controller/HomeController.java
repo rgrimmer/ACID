0c676fd21f99708adc3c601c19738be7223e131e
Maxence Guichon <maxou70@gmail.com>
2015-01-06 14:05:38 +0100

    Added load Project list from db

--- ACID-web/src/main/java/org/acid/controller/HomeController.java
+++ ACID-web/src/main/java/org/acid/controller/HomeController.java
@@ -1,15 +1,14 @@
 package org.acid.controller;
 
 import javax.ejb.EJB;
-import javax.servlet.http.HttpServletRequest;
+import org.acid.ejb.entities.Project;
 import org.acid.ejb.entities.User;
+import org.acid.ejb.entities.Board;
 import org.acid.ejb.entitymanager.EntityManager;
 import org.acid.ejb.logger.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -22,8 +21,24 @@ public class HomeController {
     private Logger logger;
 
     @RequestMapping("/home")
-    public String home(Model model) {
-        return "home";
+    public ModelAndView home(Model model) {
+        int id = 1;
+        
+        User user = entityManager.getUserById(id);
+        ModelAndView mv = new ModelAndView("home");
+        String listProject = "";
+
+        for (Project project : user.getProjectCollection()) {
+            listProject += "<div class=\"board-title\">" + project.getName() + "</div>";
+
+//            for(Board board : project.getBoardCollection()) {
+//            listProject += "<div class=\"col-sm-3\"><div class=\"panel board\"><div class=\"panel-heading\"><h3 class=\"panel-title\">"
+//                    + board.getName()
+//                    + "</h3></div></div></div>";
+//            }
+        }
+        mv.addObject("projets", listProject);
+        return mv;
     }
 
 }
