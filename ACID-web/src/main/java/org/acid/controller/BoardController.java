e6dda84300ab6ebd0cb8d2be2156fd6e730f366b
Maxence Guichon <maxou70@gmail.com>
2015-01-27 10:43:38 +0100

    Fix display task on board

--- ACID-web/src/main/java/org/acid/controller/BoardController.java
+++ ACID-web/src/main/java/org/acid/controller/BoardController.java
@@ -29,11 +29,15 @@ public class BoardController {
     @RequestMapping(value = "/board", method = RequestMethod.GET)
     public ModelAndView board(Model model, @RequestParam(required = true) Integer idBoard) {
         logger.debug("BoardController", "request get");
+        
+        Board board = entityManager.getBoardById(idBoard);
+        if(board == null)
+            return new ModelAndView("home");
+        
         ModelAndView mv = new ModelAndView("board");
         mv.addObject("idBoard", idBoard);
-        String lists = "";
-        Board board = entityManager.getBoardById(idBoard);
 
+        String lists = "";
         for (List list : board.getListCollection()) {
             lists += "<div class=\"col-sm-3\">"
                     + "<div class=\"panel panel-primary\">"
@@ -42,7 +46,11 @@ public class BoardController {
                     + "</div>"
                     + "<div id=\"draggableContainer"+  list.getIdList() +"\" class=\"panel-body\" ondrop=\"drop(event)\" ondragover=\"allowDrop(event)\">";
 
-            for (Task task : list.getTaskCollection()) {
+            for (Task task : board.getTaskCollection()) {
+                if(task.getIdList().getIdList().intValue() != list.getIdList().intValue()) {
+                    continue;
+                }
+                
                 lists += "<div id=\"draggableItem" + task.getIdTask() + "\" class=\"panel-body draggable\" draggable=\"true\" ondragstart=\"drag(event)\">"
                         + task.getDescription() + "<span class=\"glyphicon glyphicon-pencil\"></span>"
                         + "</div>";
